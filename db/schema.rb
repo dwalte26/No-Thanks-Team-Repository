361cc77e5f889bf295d78c8377e33d6d893f3ed2
Dustin <Dwalte26@uncc.edu>
2019-10-31 17:43:51 +0000

    Created Directory Employee View

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_31_170502) do
+ActiveRecord::Schema.define(version: 2019_10_31_170541) do
 
   create_table "onboardings", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2019_10_31_170502) do
     t.text "employeeid"
     t.text "email"
     t.text "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
