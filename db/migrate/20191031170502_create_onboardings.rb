2c2fe2931b0bc034f8f77f1c60e94de9a713a5da
kaitlyn miller <kmill194@uncc.edu>
2019-10-31 17:07:24 +0000

    Deleted directory model and added onboarding instead. migrated onboarding model and made schema for it

--- db/migrate/20191031170502_create_onboardings.rb
+++ db/migrate/20191031170502_create_onboardings.rb
@@ -1,9 +1,9 @@
-class CreateDirectories < ActiveRecord::Migration[5.2]
+class CreateOnboardings < ActiveRecord::Migration[5.2]
   def change
-    create_table :directories do |t|
+    create_table :onboardings do |t|
       t.string :name
       t.text :position
-      t.text :id
+      t.text :employeeid
       t.text :email
       t.text :phone
 
