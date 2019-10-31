1599e0b66bbc56c5b2a9d88c4c6448cabfddfcdd
Dustin <Dwalte26@uncc.edu>
2019-10-31 17:13:24 +0000

    Updated Directory controller

--- app/controllers/directory_controller.rb
+++ app/controllers/directory_controller.rb
@@ -1,4 +1,15 @@
 class DirectoryController < ApplicationController
-    def index
+    def show
+        @employee = Employee.find(params[:id])
     end
+    def index
+        @employees = Employee.all
+    end
+    
+    
 end
+
+private
+    def directory_params
+        params.require(:employee).permit(:name, :phone, :email)
+    end
