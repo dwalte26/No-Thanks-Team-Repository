23017c2f2898620db0b184a3ff4943dbb265cebc
Dustin <Dwalte26@uncc.edu>
2019-11-01 22:01:13 +0000

    Created Employee controller and associated views

---
+++ app/controllers/employee_controller.rb
@@ -0,0 +1,6 @@
+class EmployeeController < ApplicationController
+    def new
+    end
+    def index
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  resources :employee
+  resources :application
   
-  resources :directory do
-    resources :application
-  end
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
