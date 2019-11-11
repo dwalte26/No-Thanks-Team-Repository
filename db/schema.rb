3791f0c5dfdcdbb367af8ab078ea622d7b2eb484
kaitlyn miller <kmill194@uncc.edu>
2019-11-11 20:50:43 +0000

    Add projects routes

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_11_07_183515) do
 
-  create_table "articles", force: :cascade do |t|
-    t.string "name"
-    t.string "position"
-    t.string "employeeid"
-    t.string "email"
-    t.string "phone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "employees", force: :cascade do |t|
     t.string "name"
     t.string "position"
