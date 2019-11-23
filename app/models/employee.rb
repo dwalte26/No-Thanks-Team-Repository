3776a55d9e64ff7a0a768b960cb2d6c769e2004f
Dustin <Dwalte26@uncc.edu>
2019-11-23 02:18:30 +0000

    added password for employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,7 +3,19 @@ class Employee < ApplicationRecord
     validates :name, presence: true
     validates :position, presence: true
     validates :employeeid, presence: true
-    validates :email, presence: true
     validates :phone, presence: true
-    validates :password, presence: true
+    
+    
+    VALID_EMAIL_REGEX = /A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    before_save { self.email = email.downcase }
+    validates :email,
+        presence: true,
+        length: { maximum: 255 },
+        format: { with: VALID_EMAIL_REGEX },
+        uniqueness: { case_sensitive: false }
+        
+    validates :password,
+        presence: true,
+        length: { minimum: 6 }
+    
 end
--- db/migrate/20191101230853_create_employees.rb
+++ db/migrate/20191101230853_create_employees.rb
@@ -6,6 +6,7 @@ class CreateEmployees < ActiveRecord::Migration[5.2]
       t.string :employeeid
       t.string :email
       t.string :phone
+      t.string :password_digest
 
       t.timestamps
     end
