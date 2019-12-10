04a085d3dd521d32f838ec764a535781715ab7d7
Shane Gill <sgill14@uncc.edu>
2019-12-10 00:12:19 +0000

    Merge

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,22 @@
 
 ActiveRecord::Schema.define(version: 2019_11_26_182255) do
 
+  create_table "articles", force: :cascade do |t|
+    t.string "name"
+    t.string "position"
+    t.string "employeeid"
+    t.string "email"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "employees", force: :cascade do |t|
     t.string "name"
     t.string "position"
     t.string "employeeid"
     t.string "email"
     t.string "phone"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password"
